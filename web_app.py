57ea9386d065d76b5b13ac395f16a6e4acb644c2
akshh007 <akshayashreerajendran@gmail.com>
2025-02-26 22:05:05 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -7,16 +7,57 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # Set the page config
-st.set_page_config(page_title='Cost Calculator')
+st.set_page_config(page_title='House Price Prediction System')
 
 #loading the model
 loaded_model = pickle.load(open('Property_acquisition_cost_predictor.sav','rb'))
+district_data = {
+    "Ariyalur": 2000,
+    "Chengalpattu": 2145,
+    "Chennai": 16200,
+    "Coimbatore": 2135,
+    "Cuddalore": 2400,
+    "Dharmapuri": 2400,
+    "Dindigul": 1836,
+    "Erode": 1700,
+    "Kallakurichi": 2430,
+    "Kanchipuram": 2400,
+    "Kanniyakumari": 2430,
+    "Karur": 2430,
+    "Krishnagiri": 1836,
+    "Madurai": 2135,
+    "Mayiladuthurai": 1836,
+    "Nagapattinam": 2430,
+    "Namakkal": 2400,
+    "Nilgiris": 2400,
+    "Perambalur": 2430,
+    "Pudukkottai": 1836,
+    "Ramanathapuram": 2430,
+    "Ranipet": 1836,
+    "Salem": 2000,
+    "Sivagangai": 2400,
+    "Tenkasi": 2430,
+    "Thanjavur": 2430,
+    "Theni": 2400,
+    "Thoothukudi": 1836,
+    "Tiruchirappalli": 2430,
+    "Tirunelveli": 2430,
+    "Tirupathur": 2400,
+    "Tiruppur": 1836,
+    "Tiruvallur": 2430,
+    "Tiruvannamalai": 2400,
+    "Tiruvarur": 2430,
+    "Vellore": 2430,
+    "Viluppuram": 2400,
+    "Virudhunagar": 1836,
+    "Bangalore": 1905
+}
 
 # Sidebar for navigation
 with st.sidebar:
     selected = option_menu(
-        'Property Acquisition Cost Predictor',
-        ['About','Calculator', 'Visualiser'],
+        'House Price Prediction System',
+        ['Home','Estimate Cost', 'Visualiser'],
         default_index=0
     )
 
@@ -35,12 +76,12 @@ def hidden_price_prediction(input_data):
     finalPrice = sum(input_data)
     return finalPrice
 
-if selected == 'About':
-    st.title('Welcome to Property Acquition Cost Predictor Web App')
+if selected == 'Home':
+    st.title('House Price Prediction System')
     st.markdown(
         """
         ### 🔍 What It Does:
-        1. Predicts Property Costs: Utilizing a robust machine learning model, it accurately estimates the acquisition cost of a house based on various features like area, number of bedrooms, bathrooms, and more.
+        1. Predicts House Costs: Utilizing a robust machine learning model, it accurately estimates the cost of a house based on various features like number of bedrooms, bathrooms, and more.
         2. Calculates Hidden Costs: Beyond the base price, it calculates hidden costs such as society transfer fees, maintenance, insurance, property tax, brokerage, and sales deeds.
         3. Visualizes Housing Data: With an intuitive visualizer, users can explore and understand housing data trends through different types of plots.
 
@@ -48,31 +89,33 @@ if selected == 'About':
         - Streamlit: For an interactive and user-friendly web interface.
         - Pandas, Seaborn, Matplotlib: For data handling and visualization.
         - Numpy, Pickle: For efficient data processing and model loading.
-
-        ### 📞 Want to connect with me?
-        - [LinkedIn](https://www.linkedin.com/in/jashshah0803/)
-        - [GitHub](https://github.com/jash0803)
     """
     )
 
-if selected == 'Calculator':
+if selected == 'Estimate Cost':
     #giving a title to the page
-    st.title('Property Acquition Cost Predictor Calculator')
+    st.title('House Price Prediction System')
 
     #getting the input data from the user
-    											
+    district = st.selectbox("Select a District", list(district_data.keys()))
+    if district:
+        area = district_data[district]
+        st.write(f"The area code of {district} is {area}")
+								
     # Getting the input data from the user
     area = st.text_input('Area of the house')
-    bedrooms = st.text_input('Number of bedrooms')
-    bathrooms = st.text_input('Number of bathrooms')
-    stories = st.text_input('Number of stories')
-    mainroad = st.text_input('Is there a mainroad? (0 or 1)')
-    guestroom = st.text_input('Is there a guestroom? (0 or 1)')
-    basement = st.text_input('Is there a basement? (0 or 1)')
+    bedrooms = st.text_input('Number of Bedrooms')
+    bathrooms = st.text_input('Number of Bathrooms')
+    stories = st.text_input('Number of Storey')
+    mainroad = st.text_input('Is there a Mainroad? (0 or 1)')
+    guestroom = st.text_input('Is there a Guestroom? (0 or 1)')
+    basement = st.text_input('Is there a Basement? (0 or 1)')
     hotwaterheating = st.text_input('Is there hot water heating? (0 or 1)')
     airconditioning = st.text_input('Is there air conditioning? (0 or 1)')
     parking = st.text_input('Number of parking spaces')
     prefarea = st.text_input('Is there a preferred area? (0 or 1)')
+    
+    sclarea = st.text_input('School / College nearby (0 or 1)')
     furnishingstatus = st.text_input('Furnishing status (unfurnished-0, semi-furnished-1, or furnished-2)')
 
     # Initialize session state for price and hiddenPrice
@@ -82,7 +125,7 @@ if selected == 'Calculator':
         st.session_state.hiddenPrice = 0
 
     #creating a button
-    if st.button('Predict the price of your house'):
+    if st.button('Predict the price of the house'):
         try:
             area = int(area)
             bedrooms = int(bedrooms)
@@ -97,7 +140,7 @@ if selected == 'Calculator':
             prefarea = int(prefarea)
             furnishingstatus = int(furnishingstatus)
             st.session_state.price = house_price_prediction([area,bedrooms,bathrooms,stories,mainroad,guestroom,basement,hotwaterheating,airconditioning,parking,prefarea,furnishingstatus])
-            st.success(f'The price of your house is: {st.session_state.price}')
+            st.success(f'The price of the house is: {st.session_state.price}')
         except ValueError:
             st.error("Please enter valid numerical values for all inputs.")
 
@@ -124,8 +167,8 @@ if selected == 'Calculator':
             propertyTax = float(area) * propertyTax
             brokerFee = int(brokerFee)
             st.session_state.hiddenPrice = hidden_price_prediction([transferFee,maintainance,homeInsurance,salesDeed,propertyTax,brokerFee,st.session_state.price])
-            st.success(f'The initial cost of your house: {st.session_state.price}')
-            st.success(f'The final cost of your house in a year is: {st.session_state.hiddenPrice}')
+            st.success(f'The initial cost of the house: {st.session_state.price}')
+            st.success(f'The final cost of the house in a year is: {st.session_state.hiddenPrice}')
         except ValueError:
             st.error("Please enter valid numerical values for all inputs.")
 
